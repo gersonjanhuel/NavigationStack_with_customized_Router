ef9378945c9efc4696ef9f236c38208261a6a3b4
Gerson Janhuel <gersonjanhuel@gmail.com>
2022-12-07 15:50:59 +0700

    Initial Commit

---
+++ TrialNavigationStack/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TrialNavigationStack
+//
+//  Created by Gerson Janhuel on 07/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrialNavigationStack/TrialNavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrialNavigationStackApp.swift
+//  TrialNavigationStack
+//
+//  Created by Gerson Janhuel on 07/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TrialNavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
